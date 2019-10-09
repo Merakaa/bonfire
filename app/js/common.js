da08a95f3b19d146120f2fa27d7e1cc3bc78fd93
Merak <mesheryakov97@gmail.com>
2019-10-09 13:28:12 +0500

    finish

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,13 @@
 $(function() {
 
-	// Custom JS
-
+	// burger
+	 
+	document.querySelector('.burger-menu').onclick = function() {
+		this.classList.toggle('active'); 
+	}
+	 
+	$(".burger-menu").click(function(){
+		$(".mobile-menu").fadeToggle();
+	})
+	 
 });
